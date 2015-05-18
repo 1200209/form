20291a89841acd42f6f8b8e751dcd703732dd680
sasamoto <s1200209.aizu@gmail.com>
2015-05-19 02:10:26 +0900

    body.form.btn -> body.btn

--- form.js
+++ form.js
@@ -5,12 +5,9 @@ var target = document.getElementById('body');
 newElement.appendChild(newtxt);
 newElement.id = 'test';
 target.appendChild(newElement);
-var form = document.createElement('form');
-from.id = 'input';
-target.appendChild(form);
 var add = document.createElement('button');
 add.textContent = '追加';
-form.appendChild(add);
+target.appendChild(add);
 
 }
 
